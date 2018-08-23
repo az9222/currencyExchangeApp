94112ea06094f34919b4e77ad6bfad318fd16d3a
Angela Zhou <angelazhou922@gmail.com>
2018-08-23 16:26:52 -0400

    Functions work

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //the first parameter is conversion from, second is conversion to
 
 $(document).ready(function(){
-    function receiveConversionData (convertFrom, convertTo) {
+    function receiveConversionData (convertFrom, convertTo, amount) {
         $.ajax({
             url: "https://api.exchangeratesapi.io/latest?base="+convertFrom,
             method: "GET" ,
@@ -10,18 +10,24 @@ $(document).ready(function(){
             contentType: 'application/JSON',
             success: function(data){
                 let conversionRate = data.rates[convertTo];
-                 $(".conversion-container").append("conversionRate")
+                let convertedAmt = (conversionRate*amount).toFixed(2)
+                 $("#amount").append("$"+(convertedAmt))
             }
         })
     }
     // receiveConversionData("USD", "GBP");
 
-$(".btn").click(function(){
-    let countryNameConvertFrom = $('#start-country option:selected').attr('value');
-    let conversionInput = Number.parseInt($("#amount-convert-from").val());
-    let countryNameConvertTo = $('#end-country option:selected').attr('value');
-    let conversionAmount = receiveConversionData(countryNameConvertFrom, countryNameConvertTo) * conversionInput;
-    alert(conversionAmount);
-})
+    $(".btn").click(function(){
+        $("#amount").text('');
+        let countryNameConvertFrom = $('#start-country option:selected').attr('value');
+        let conversionInput = Number.parseFloat($("#amount-convert-from").val());
+        let countryNameConvertTo = $('#end-country option:selected').attr('value');
+        if (countryNameConvertFrom === countryNameConvertTo) {
+            $("#amount").append("$"+conversionInput);
+            alert("Change country!")
+        } else {
+        receiveConversionData(countryNameConvertFrom, countryNameConvertTo, conversionInput);
+        }
+    })
 
 })
